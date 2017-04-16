c32031498c7e2e7cb296bbf45099b618db42c19b
harwinvoid <harwinyang@gmail.com>
2017-04-16 17:28:46 +0800

    增加悬浮操作组件

---
+++ src/components/popButton.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="popBtn" ref="popBtn" @click="btnClick" :style='position'>
+  <span class="popBtn__content" v-html='content'></span>
+</div>
+</template>
+
+<script>
+  export default {
+    props: ['content', 'position'],
+    methods: {
+      btnClick () {
+        this.$emit('handleClick')
+      }
+    }
+
+  }
+</script>
+
+<style lang='scss'>
+  .popBtn {
+    position: fixed;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+    z-index: 3;
+    background: rgba(0,0,0,0.5);
+    border-radius: 50%;
+    @at-root #{&}__content {
+      line-height: 40px;
+      color: #fff;
+      font-size: 8px;
+    }
+  }
+</style>
