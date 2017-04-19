cf6e50d9cce5450e670770487f8c959aec504f5d
harwinvoid <harwinyang@gmail.com>
2017-04-19 10:45:39 +0800

    新增评论页面

---
+++ src/components/Reply.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="markdown-body">
+    <ul>
+      <li v-for='reply in replies' v-html="reply.content">
+      <li/>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        replies: []
+      }
+    },
+    beforeMount () {
+      this.replies = this.$store.state.currentTopicReplies
+    }
+  }
+</script>
+
+<style lang="">
+
+</style>
