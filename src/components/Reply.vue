fb4370d1288b33a10c913c98f5c40860b9d7bdd5
harwinvoid <harwinyang@gmail.com>
2017-04-19 17:53:45 +0800

    修改评论页面不

--- src/components/Reply.vue
+++ src/components/Reply.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="markdown-body">
-    <ul>
-      <li v-for='reply in replies' v-html="reply.content">
-      <li/>
-    </ul>
+  <div class="reply">
+    <header class="reply__header">
+      评论（{{replies.length}}）
+    </header>
+    <main class="markdown-body">
+      <ul class="reply__list">
+        <reply-item v-for='reply in replies' :item='reply'>
+        </reply-item>
+      </ul>
+    </main>
   </div>
 </template>
 <script>
+  import ReplyItem from '@/components/ReplyItem'
   export default {
     data () {
       return {
         replies: []
       }
+    },
+    components: {
+      ReplyItem
     },
     beforeMount () {
       this.replies = this.$store.state.currentTopicReplies
@@ -19,6 +28,25 @@
   }
 </script>
 
-<style lang="">
+<style lang="scss">
+.reply {
+  font-size: 10px;
+  @at-root #{&}__header {
+    background: #4A90E2;
+    height: 45px;
+    line-height: 45px;
+    font-weight: bold;
+    font-size:  14px;
+    color: #fff;
+  }
+  .markdown-body {
+    padding: 15px;
+  }
+  @at-root #{&}__list {
+    list-style: none;
+    padding-left: 0 !important;
+    font-size: 10px;
+  }
+}
 
 </style>
---
+++ src/components/ReplyItem.vue
@@ -0,0 +1,95 @@
+<template>
+   <li class="reply__item">
+     <div class="user__avatar">
+      <img class="user__avatar"
+           :src="item.author.avatar_url"
+           :title="item.author.loginname"
+           :alt="item.author.loginname">
+    </div>
+    <div class="reply-info">
+      <div class="reply-info__meta">
+        <p>{{item.author.loginname}}</p>
+        <time>{{createTime}}</time>
+      </div>
+      <p class="reply-info__content" v-html='item.content'></p>
+    </div>
+    <div class="action-box-wrap">
+      <div class="action-box">
+      <span class="action-box__reply"><i class="fa fa-commenting-o" aria-hidden="true"></i></span>
+      <span class="action-box__like"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></span>
+    </div>
+    </div>
+
+   </li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  computed: {
+    createTime () {
+      return this.moment(this.item.create_at).fromNow()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .reply__item {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 15px 0;
+    &:not(:last-child) {
+      border-bottom: solid 1px #eee;
+    }
+    .reply-info {
+      width: 84%;
+      align-self: center;
+      text-align: left;
+      p {
+        margin-bottom: 0 !important;
+      }
+      @at-root #{&}__meta {
+        position: relative;
+        margin-bottom: 5px;
+        p {
+          font-weight: bold
+        }
+        time {
+          position: absolute;
+          right: 0;
+          top: 0;
+        }
+      }
+      @at-root #{&}__content {
+        font-size: 10px;
+      }
+    }
+
+    .action-box-wrap {
+      width: 100%;
+      position: relative;
+      height: 30px;
+      margin-top: 10px;
+      .action-box {
+        position: absolute;
+        right: 0;
+        top: 0;
+        line-height: 30px;
+        height: 30px;
+        border: 1px solid #eee;
+        border-radius: 5px;
+        text-align: left;
+        span {
+          width: 45px;
+          text-align: center;
+          display: inline-block;
+          &:not(:last-child) {
+            border-right: solid 1px #eee;
+          }
+      }
+    }
+  }
+}
+
+</style>
